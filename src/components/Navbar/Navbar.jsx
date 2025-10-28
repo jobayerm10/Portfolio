d417460a49b2e6247c3f0a2404f32c2c73917905
CodeContainer <jobayermahmud976@gmail.com>
2025-10-28 11:54:35 +0600

    alignment done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,21 +4,22 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between mt-3 m-4">
       <img
         src={logo}
         className="w-15 h-15 object-contain"
         alt="Jobayer Mahmud"
       />
-      <div className="flex">
-        <ul>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/experience">Experience</NavLink>
-          <NavLink to="/work">Work</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
-          <NavLink to="/blog">Blog</NavLink>
-        </ul>
-        <button className="btn btn-primary px-10">Login</button>
+      <div className=" flex justify-center items-center gap-10 relative w-full text-accent">
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/experience">Experience</NavLink>
+        <NavLink to="/work">Work</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/blog">Blog</NavLink>
+
+        <button className="btn btn-primary px-10 absolute right-0">
+          Resume
+        </button>
       </div>
     </div>
   );
