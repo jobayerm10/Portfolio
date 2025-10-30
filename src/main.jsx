7585615ec55ae083ec2bc18ce17467edfcc02d3f
CodeContainer <jobayermahmud976@gmail.com>
2025-10-30 13:31:55 +0600

    Build fixed and ready for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,36 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Navbar></Navbar>,
-//   },
-//   {
-//     path: "/about",
-//     element: <Header></Header>,
-//   },
-// ]);
-
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <RouterProvider router={router}>
-//       <App></App>
-//     </RouterProvider>
-//   </StrictMode>
-// );
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Navbar></Navbar>,
-//   },
-//   {
-//     path: "/about",
-//     element: <Header></Header>,
-//   },
-// ]);
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App></App>
